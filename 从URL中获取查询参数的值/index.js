e13cdc8cd902e917faacb4e20ea2e60e31c7b9f5
Songnian <1127093059@qq.com>
2022-04-04 11:56:03 +0800

    防抖

--- 从URL中获取查询参数的值/index.js
+++ 从URL中获取查询参数的值/index.js
@@ -1,5 +1,5 @@
 function getQueryVariable(variable) {
-    var query = window.location.search.substr(1);
+    var query = window.location.search.substring(1);
     var vars = query.split("&");
     for (var i = 0; i < vars.length; i++) {
         var pair = vars[i].split("=");
