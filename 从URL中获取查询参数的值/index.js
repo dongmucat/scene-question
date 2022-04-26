8f84b3420b27c9a63e4325b041d879abfa95d11d
Songnian <1127093059@qq.com>
2022-04-26 19:16:03 +0800

    适用于浏览器原生

--- 从URL中获取查询参数的值/index.js
+++ 从URL中获取查询参数的值/index.js
@@ -1,8 +1,11 @@
 function getQueryVariable(variable) {
-    var query = window.location.search.substring(1);
-    var vars = query.split("&");
-    for (var i = 0; i < vars.length; i++) {
-        var pair = vars[i].split("=");
+    /* substring除去? */
+    let query = window.location.search.substring(1);
+    /* 分离 */
+    let vars = query.split("&");
+    for (let i = 0; i < vars.length; i++) {
+        /* 分离 */
+        let pair = vars[i].split("=");
         if (pair[0] == variable) {
             return pair[1];
         }
