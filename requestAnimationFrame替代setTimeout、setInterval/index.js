d2526fb0365d558470ad06e3eb9b0d9ea703917d
Songnian <1127093059@qq.com>
2022-10-23 17:36:58 +0800

    udp：优化了code

--- requestAnimationFrame替代setTimeout、setInterval/index.js
+++ requestAnimationFrame替代setTimeout、setInterval/index.js
@@ -1,46 +1,64 @@
 class RAF {
+	/**
+	 * 构造函数
+	 */
 	constructor() {
 		this.timeoutTimer = null;
 		this.intervalTimer = null;
 	}
-
+	/**
+	 * requestAnimationFrame实现setTimeout
+	 * @param {function} cb 回调函数
+	 * @param {number} delay 延时
+	 * @returns setTimeout ID
+	 */
 	setTimeout = (cb, delay) => {
-		let now = Date.now;
-		let stime = now();
-		let etime = stime;
+		let stime = Date.now();
+		let etime = null;
 		let loop = () => {
-			this.timeoutTimer = requestAnimationFrame(loop);
-			etime = now();
+			etime = Date.now();
 			if (etime - stime >= delay) {
 				cb();
 				cancelAnimationFrame(this.timeoutTimer);
 			}
+			this.timeoutTimer = requestAnimationFrame(loop);
 		};
 		this.timeoutTimer = requestAnimationFrame(loop);
 		return this.timeoutTimer;
 	};
 
+	/**
+	 * 清除 setTimeout 定时器
+	 */
 	clearTimout = () => {
 		cancelAnimationFrame(this.timeoutTimer);
 	};
 
+	/**
+	 * requestAnimationFrame实现setInterval
+	 * @param {function} cb 回调函数
+	 * @param {number} delay 延时
+	 * @returns setInterval ID
+	 */
 	setInterval = (cb, delay) => {
 		// 实现setInterval功能
-		let now = Date.now;
-		let stime = now();
-		let etime = stime;
+		let stime = Date.now();
+		let etime = null;
 		let loop = () => {
-			this.intervalTimer = requestAnimationFrame(loop);
-			etime = now();
+			etime = Date.now();
 			if (etime - stime >= delay) {
-				stime = now();
-				etime = stime;
+				stime = Date.now();
 				cb();
 			}
+			this.intervalTimer = requestAnimationFrame(loop);
 		};
 		this.intervalTimer = requestAnimationFrame(loop);
 		return this.intervalTimer;
 	};
+
+	/**
+	 * 清除 setInterval 定时器
+	 */
 	clearInterval = () => {
 		cancelAnimationFrame(this.intervalTimer);
 	};
